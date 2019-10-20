ac59a0768e50728eabbefd457a63c636c73eb035
Eran Cohen <ceranco@gmail.com>
2019-10-20 20:55:12 +0300

    Added basic input handling.

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,26 @@ use ggez::event;
 use ggez::event::EventHandler;
 use ggez::graphics;
 use ggez::graphics::{Color, DrawMode, Mesh, Rect};
-use ggez::mint::Point2;
+use ggez::input;
+use ggez::mint::{Point2, Vector2};
 use ggez::{Context, ContextBuilder, GameResult};
 
-struct Snake {
-    mesh: Mesh,
-    position: (usize, usize),
-}
-
-impl Snake {
-    fn draw(&mut self, ctx: &mut Context, cell_size: &(usize, usize)) -> GameResult {
-        let point = Point2 {
-            x: (self.position.0 * cell_size.0) as f32,
-            y: (self.position.1 * cell_size.1) as f32,
-        };
-
-        graphics::draw(ctx, &mut self.mesh, (point,))?;
-        Ok(())
-    }
-}
+mod snake;
+use snake::Snake;
 
 struct Game {
-    grid: (usize, usize),
-    cell: (usize, usize),
+    grid: (i32, i32),
+    cell: (i32, i32),
     update_interval: u16,
-
+    last_update: u128,
     snake: Snake,
+    velocity: Vector2<i32>,
 }
 
 impl Game {
     fn new(ctx: &mut Context) -> GameResult<Self> {
-        let cell = (64usize, 64usize);
-        let grid = (32usize, 32usize);
+        let cell = (64, 64);
+        let grid = (32, 32);
 
         let snake_mesh = Mesh::new_rectangle(
             ctx,
@@ -45,17 +33,31 @@ impl Game {
         Ok(Self {
             snake: Snake {
                 mesh: snake_mesh,
-                position: (0, 0),
+                position: Point2 { x: 0, y: 0 },
             },
+            velocity: Vector2 { x: 1, y: 0 },
             cell: cell,
             grid: grid,
             update_interval: 1000,
+            last_update: 0,
         })
     }
 }
 
 impl EventHandler for Game {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        let current_time = ggez::timer::time_since_start(ctx).as_millis();
+        let delta = (current_time - self.last_update) as u16;
+        if delta >= self.update_interval {
+            let new_position = Point2 {
+                x: self.snake.position.x + self.velocity.x,
+                y: self.snake.position.y + self.velocity.y,
+            };
+            self.snake.update(new_position)?;
+
+            self.last_update = current_time;
+        }
+
         Ok(())
     }
 
@@ -64,6 +66,26 @@ impl EventHandler for Game {
         self.snake.draw(ctx, &self.cell)?;
         graphics::present(ctx)?;
         Ok(())
+    }
+
+    fn key_down_event(
+        &mut self,
+        ctx: &mut Context,
+        keycode: event::KeyCode,
+        _keymods: input::keyboard::KeyMods,
+        _repeat: bool,
+    ) {
+        if keycode == event::KeyCode::Escape {
+            event::quit(ctx);
+        }
+
+        self.velocity = match keycode {
+            event::KeyCode::Up => Vector2 { x: 0, y: -1},
+            event::KeyCode::Down => Vector2 { x: 0, y: 1},
+            event::KeyCode::Left => Vector2 { x: -1, y: 0},
+            event::KeyCode::Right => Vector2 { x: 1, y: 0},
+            _ => self.velocity
+        };
     }
 }
 
